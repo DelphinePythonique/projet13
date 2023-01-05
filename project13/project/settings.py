940a00e882495880bd165f9bbd1b299d77bdb6b7
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2023-01-05 11:41:49 +0100

    FIX/ test circle ci security issue

--- project13/project/settings.py
+++ project13/project/settings.py
@@ -127,7 +127,6 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
