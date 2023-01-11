026eb69a255850d84bf6828ea26456d7fc375f93
DelphinePythonique <delphine.lemire+openclassroom@gmail.com>
2023-01-11 12:34:43 +0100

    FIX/ circle flake8 config

--- project13/lettings/models.py
+++ project13/lettings/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.utils.translation import gettext_lazy as _
-import os
 
 
 # Create your models here.
